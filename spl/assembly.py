54db82c3e6ff1816aef6463cb427fd1fed675a60
Álvaro Andrés Romero Castro <alromeroca@unal.edu.co>
2026-04-28 22:42:48 -0500

    Nuevo assembler simplificado

--- spl/assembly.py
+++ spl/assembly.py
@@ -2,6 +2,7 @@ import sys
 from ply import lex
 from enum import Enum, auto
 from struct import pack #para conversion a binario
+from math import ceil
 
 REGISTERS = {
     'PC': 0x1, 'SP': 0x2, 'BP': 0x3, 'IR': 0x4, 
@@ -48,17 +49,11 @@ class ParamType(Enum):
         
 class Builder:
     _readableModes: dict[str, str] = {
-        "b":            "0b{0:064b}\n",
-        "bin":          "0b{0:064b}\n",
-        "binary":       "0b{0:064b}\n",
-        "o":            "0o{0:022o}\n",
-        "oct":          "0o{0:022o}\n",
-        "octal":        "0o{0:022o}\n",
-        "hex":          "0x{0:016X}\n",
-        "hexadecimal":  "0x{0:016X}\n",
-        "d":            "{0:d}\n",
-        "dec":          "{0:d}\n",
-        "decimal":      "{0:d}\n"
+        "b":            ("0b{0:{1}b}", 1),
+        "bin":          ("0b{0:{1}b}", 1),
+        "binary":       ("0b{0:{1}b}", 1),
+        "hex":          ("0x{0:{1}X}", 4),
+        "hexadecimal":  ("0x{0:{1}X}", 4),
     }
     
     _jumps: set[str] = {
@@ -73,22 +68,18 @@ class Builder:
         "JMPNANDZ"
     }
     
-    def __init__(self, readable: bool | str = False):
-        if readable == True: self._readable = self._readableModes["b"]
-        else: self._readable = self._readableModes.get(readable, False)
-        self._writeMode = "w" if readable else "wb"
+    def __init__(self, format: str = "b"):
+        if format.lower() not in self._readableModes: raise ValueError(f"Error: formato \"{format}\" no reconocido")
+        self._format = self._readableModes[format.lower()]
         
         self._sectionIsText: bool = True
         self._expect: None | str = None
         
-        self._textOutput: list[int] = []
-        self._textReplace: dict[str,list[tuple[int,int]]] = {}
-        self._textDirs: dict[int,list[tuple[int, int]]] = {}
+        self._textOutput: list[str] = []
+        self._dataOutput: list[str] = []
         
-        self._dataOutput: list[int] = []
-        self._dataReplace: dict[str,list[int]] = {}
-        
-        self._outputLabels: list[tuple[str, int] | int] = [0]
+        self._textLabels: list[tuple[str, int]] = []
+        self._dataLabels: list[tuple[str, int]] = []
         self._inst: str = None
         self._instBase: int = None
         self._expectedParams: ParamType = None
@@ -97,15 +88,18 @@ class Builder:
         self._labels: dict[str, str] = {}
         self._hasErrors = False
     
-    def encodeInt(self, val: int) -> bytes | str:
-        if self._readable:
-            return self._readable.format(val)
-        return val.to_bytes(8)
+    def encodeInt(self, val: int, size: int | None = None) -> str:
+        if size:
+            ones = (1 << size) - 1
+            if val < 0:
+                val = ones ^ (val & ones) + 1 #complemento de 2
+            else:
+                val &= ones
+        
+        return self._format[0].format(val, f"0{ceil(size / self._format[1])}" if size is not None else "") 
     
-    def encodeStr(self, val: str) -> bytes | str:
-        if self._readable:
-            return f'"{val}\\0"\n'
-        return val.encode("utf-8") + b"\0"
+    def encodeReplace(self, label: str, size: int) -> str:
+        return "{" + label + ":" + self.encodeInt(size) + "}"
     
     def checkExpected(self, pos: str):
         if not self._expect:
@@ -154,13 +148,11 @@ class Builder:
         if not self._sectionIsText:
             self._expect = ","
             if type == "ID":
-                if value in self._dataReplace:
-                    self._dataReplace[value].append(len(self._dataOutput))
-                else:
-                    self._dataReplace[value] = [len(self._dataOutput)]
-                value = 0
+                value = self.encodeReplace(value)
             elif type == "FLOAT": #convertir floante a representacion binaria
-                value = int.from_bytes(pack(">d", value))
+                value = self.encodeInt(int.from_bytes(pack(">d", value)),64)
+            else:
+                value = self.encodeInt(value, 64)
             self._dataOutput.append(value)
             return
         
@@ -202,11 +194,10 @@ class Builder:
             return
         
         self._labels[label] = pos
-        oLen = len(self._textOutput if self._sectionIsText else self._dataOutput)
         if self._sectionIsText:
-            self._outputLabels.insert(0, (label, oLen))
+            self._textLabels.append((label, len(self._textOutput)))
         else:
-            self._outputLabels.append((label, oLen))
+            self._dataLabels.append((label, len(self._dataOutput)))
     
     def clearInstruction(self) -> None:
         self._inst = None
@@ -215,7 +206,7 @@ class Builder:
         self._paramsLen = None
         self._params = []
     
-    def instructionEnd(self, pos):
+    def instructionEnd(self, pos: str):
         if self._expect == ",": self._expect = None
         
         if self._inst == None or not self._sectionIsText:
@@ -227,102 +218,64 @@ class Builder:
             self.clearInstruction()
             return
         
+        encoded: list[str] = []
         word = 0
         nParams = len(self._params)
         accOcupied = sum(self._paramsLen[:nParams-1:-1])
+        accStored = 0
         for param, pLen in zip(self._params[::-1], self._paramsLen[nParams-1::-1]):
-            pos = ((len(self._textOutput) + 1) << 6) - accOcupied - pLen
-            if not isinstance(param, str):
-                if isinstance(param, float):
-                    #convertir float a representacion binaria
-                    significant = int.from_bytes(pack(">d", param))
-                    #truncar float para guardar bits mas significativos
-                    significant >>= 64 - pLen
-                    param = significant
-                
-                if self._inst in self._jumps:
-                    # si es una instruccion de salto y no es una id
-                    if param in self._textDirs:
-                        self._textDirs[param].append((pos, pLen))
-                    else:
-                        self._textDirs[param] = [(pos, pLen)]
-                else:
-                    word |= (param & self.len2mask(pLen)) << accOcupied
-            else: # si es un identificador
-                if param in self._textReplace:
-                    self._textReplace[param].append((pos, pLen))
-                else:
-                    self._textReplace[param] = [(pos, pLen)]
+            if isinstance(param, str) or self._inst in self._jumps:
+                if accOcupied > 0:
+                    encoded.insert(0, self.encodeInt(word, accOcupied - accStored))
+                encoded.insert(0, self.encodeReplace(param, pLen))
+                word = 0
+                accOcupied += pLen
+                accStored = accOcupied
+                continue
+                    
+            if isinstance(param, float):
+                #convertir float a representacion binaria
+                significant = int.from_bytes(pack(">d", param))
+                #truncar float para guardar bits mas significativos
+                significant >>= 64 - pLen
+                param = significant
+            
+            word |= (param & self.len2mask(pLen)) << accOcupied - accStored
             accOcupied += pLen
-        word |= self._instBase << accOcupied
-        word &= self.len2mask(64)
-        self._textOutput.append(word)
+        word |= self._instBase << accOcupied - accStored
+        encoded.insert(0, self.encodeInt(word, 64 - accStored))
+        self._textOutput.append("".join(encoded))
         
         self.clearInstruction()
     
     def len2mask(self, len: int):
         return (1 << len) - 1
     
-    def encodeLabels(self) -> bytes:
-        ret = "" if self._readable else b""
-        for val in self._outputLabels:
-            if val != 0:
-                lbl, pos = val
-                ret += self.encodeStr(lbl)
-                ret += self.encodeInt(pos)
-            else:
-                ret += self.encodeStr("")
-        ret += self.encodeStr("")
+    def encodeLabels(self, section: str, labels: list[tuple[str, int]]) -> str:
+        if not labels: return ""
+        ret = section + "Labels {\n"
+        ret += "".join(f"{label}: {self.encodeInt(pos)}\n" for label, pos in labels)
+        ret += "}\n"
+        
         return ret
     
-    def encodeSectionOutput(self, output: list[bytes]) -> bytes:
-        ret = "" if self._readable else b""
-        ret += self.encodeInt(len(output))
-        for inst in output:
-            ret += self.encodeInt(inst)
-        return ret
-    
-    def encodeText(self) -> bytes:
-        ret = "" if self._readable else b""
-        
-        #codificar identificadores a reemplazar en la seccion texto
-        for id, posInfo in self._textReplace.items():
-            ret += self.encodeStr(id)
-            ret += self.encodeInt(len(posInfo))
-            for start, posLen in posInfo:
-                ret += self.encodeInt(start)
-                ret += self.encodeInt(posLen)
-        ret += self.encodeStr("")
-        
-        #codificar direcciones en la seccion texto
-        ret += self.encodeInt(len(self._textDirs))
-        for dir, posInfo in self._textDirs.items():
-            ret += self.encodeInt(dir)
-            ret += self.encodeInt(len(posInfo))
-            for start, posLen in posInfo:
-                ret += self.encodeInt(start)
-                ret += self.encodeInt(posLen)
-        
-        #codificar instrucciones
-        ret += self.encodeSectionOutput(self._textOutput)
-        return ret
-        
-    def encodeData(self) -> bytes:
-        ret = "" if self._readable else b""
-        for id, posInfo in self._dataReplace.items():
-            ret += self.encodeStr(id)
-            ret += self.encodeInt(len(posInfo))
-            for pos in posInfo:
-                ret += self.encodeInt(pos)
-        ret += self.encodeStr("")
-        ret += self.encodeSectionOutput(self._dataOutput)
+    def encodeSectionOutput(self, section: str, output: list[str]) -> str:
+        if not output: return ""
+        ret = section+"Section: " + self.encodeInt(len(output)) + "\n"
+        ret += "\n".join(output)
         return ret
     
     def write(self, file: str):
-        with open(file, self._writeMode) as out:
-            out.write(self.encodeLabels())
-            out.write(self.encodeText())
-            out.write(self.encodeData())
+        if self._hasErrors:
+            print("No se pudo escribir el archivo debido a que contiene errores")
+            return
+        with open(file, "w") as out:
+            out.write(self.encodeLabels("Text", self._textLabels))
+            out.write(self.encodeLabels("Data", self._dataLabels))
+            
+            out.write(self.encodeSectionOutput("Text", self._textOutput))
+            out.write("\n")
+            out.write(self.encodeSectionOutput("Data", self._dataOutput))
             
 current_file = ""
 lastNewLinePos = 0
@@ -354,8 +307,8 @@ INSTRUCTION: dict[str, tuple[ParamType, int, list[int]]] = {
     "SHFTR":    (ParamType.rr,  0x00000000F00360,   [4, 4]),
     "ABVAL":    (ParamType.rr,  0x00000000000041,   [4, 4]),
     "CHNSGN":   (ParamType.rr,  0x00000000000042,   [4, 4]),
-    "CHNINT":    (ParamType.rr, 0x00000000000043,   [4, 4]),
-    "CHNFLT":    (ParamType.rr, 0x00000000000044,   [4, 4]),
+    "CHNINT":   (ParamType.rr,  0x00000000000043,   [4, 4]),
+    "CHNFLT":   (ParamType.rr,  0x00000000000044,   [4, 4]),
     "ADD":      (ParamType.rrr, 0x0000000000001,    [4, 4, 4]),
     "SUB":      (ParamType.rrr, 0x0000000000002,    [4, 4, 4]),
     "MUL":      (ParamType.rrr, 0x0000000000003,    [4, 4, 4]),
@@ -444,7 +397,7 @@ def t_label_eof(t):
     print(f"Error en {current_file}:{t.lexer.lineno}:{t.lexer.lexpos-lastNewLinePos}: final de archivo alcanzado antes de cerrar etiqueta")
     return None
 
-t_label_LBL_DEF = r"\w[\d\w]+"
+t_label_LBL_DEF = r"\w[\d\w]*"
 
 
 def t_ID(t):
@@ -472,19 +425,12 @@ def t_INSTEND(t):
 
 def main():
     if len(sys.argv) < 3:
-        print("Uso: python assembly.py <archivo_asm> <archivo_salida.o>")
+        print("Uso: python assembly.py <archivo_asm> <archivo_salida.o> [b|bin|binary|hex|hexadecimal]")
         sys.exit(1)
     asm_file = sys.argv[1]
     out_file = sys.argv[2]
     
-    readable = None
-    try:
-        index = sys.argv.index("--readable")
-        if index < len(sys.argv):
-            readable = sys.argv[index + 1]
-        else: readable = True
-    except ValueError:
-        readable = False
+    format = sys.argv[3] if len(sys.argv) > 3 else "b"
 
     global current_file
     current_file = asm_file
@@ -497,7 +443,7 @@ def main():
     
     lexer = lex.lex()
     lexer.input(lines)
-    builder = Builder(readable)
+    builder = Builder(format)
     last_pos = f"{asm_file}:{1}:{1}"
     while tok := lexer.token():
         # if tok.type in ["INST", ","]: tok.value
@@ -548,7 +494,7 @@ class Assembler:
         builder = self.assemble_text(text)
 
         # Forzar modo legible (IMPORTANTE)
-        builder._readable = builder._readableModes["dec"]
+        builder._format = builder._readableModes["dec"]
 
         output = ""
 
